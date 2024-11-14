7501f547887e2a4778c5b6748ed4b5ca4c57e140
Julio <julio@hoxo.com.br>
2024-11-14 13:44:37 -0300

    build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,40 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import typescript2 from "rollup-plugin-typescript2";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts"; 
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.ts", 
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: "cjs", 
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: "esm", 
         sourcemap: true,
       },
     ],
     plugins: [
-      peerDepsExternal(),
+      peerDepsExternal(), 
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
+      typescript(), 
+      terser(), 
     ],
-    external: ["react", "react-dom"],
+    external: ["react", "react-dom"], 
   },
   {
-    input: "dist/esm/types/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts.default()],
+    input: "src/index.ts", 
+    output: [{ file: "dist/index.d.ts", format: "esm" }], 
+    plugins: [typescript2(), dts()],
   },
 ];
